f9c0560850f17eb9978485e077711b6a75cf5a98
Baburao Adkane <b.g.adkane27@gmail.com>
2025-05-13 23:58:00 +0530

    aos animation index.html hero-section modified

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const NAV_LINKS = [
+    { label: "WORK", href: "#work" },
+    { label: "EXPERIENCE", href: "#experience" },
+    { label: "SKILLS", href: "#skills" },  
+    { label: "TESTIMONIALS", href: "#testimonials" },
+    { label: "CONTACT ME", href: "#contact" },
+  ];
+  
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,35 +1,53 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import Model from "../components/Model";
+import { NAV_LINKS } from "../constants/index";
 
 const HeroSection = () => {
   return (
     <div className="hero-container">
       <Canvas camera={{ position: [0, 0, -2], fov: 60 }}>
         <ambientLight intensity={0.5} />
-        <directionalLight position={[2, 2, 5]} />        
-        <Stars radius={100} depth={50} count={800} factor={4} />        
+        <directionalLight position={[2, 2, 5]} />
+        <Stars radius={100} depth={50} count={800} factor={4} />
         <Model />
-        <OrbitControls 
-        enableZoom={false} 
-        />
+        <OrbitControls enableZoom={false} />
       </Canvas>
 
       <div className="hero-content">
-        <h1 className="uppercase text-nowrap" data-aos="fade-up" data-aos-duration="2000"><strong>Baburao Adkane</strong></h1>
-        <div>
-          <p className="uppercase font-semibold sm:text-xl text-[14px]">QA/QC ENGINEER</p>
+        <h1
+          className="uppercase text-nowrap font-extrabold"
+          data-aos="fade-up"
+          data-aos-duration="2000"
+        >
+          <strong>Baburao Adkane</strong>
+        </h1>
+
+        <div data-aos="fade-up" data-aos-duration="2000" data-aos-delay="400">
+          <p className="uppercase font-semibold sm:text-xl text-[14px]">
+            QA/QC ENGINEER
+          </p>
         </div>
-        <p className="uppercase leading-8 text-[10px] sm:text-base bg-fuchsia-600/10 p-1">ATTENTIVE | STRONG ANALYTICAL SKILL | <br /> 
-        Excellent Communication & Collaboration | Process-Oriented |<br />
-        Curious & Proactive</p>
-        <div className="hero-links">
-          <a href="#work">WORK</a>
-          <a href="#experience">EXPERIENCE</a>
-          <a href="#skills">SKLLS</a>
-          <a href="#testimonials">TESTIMONIALS</a>
-          <a href="#contact">CONTACT ME</a>
-        </div>
+
+        <p
+          className="uppercase leading-8 text-[10px] sm:text-sm text-[#fff] font-semibold bg-cyan-500/20 py-2 rounded-md"
+          data-aos="zoom-in"
+          data-aos-duration="2000"
+          data-aos-delay="600"
+        >
+          ATTENTIVE | STRONG ANALYTICAL SKILL | <br />
+          Excellent Communication & Collaboration | Process-Oriented |<br />
+          Curious & Proactive
+        </p>
+        <ul className="flex flex-row md:flex-row flex-wrap justify-center gap-6 md:gap-14 tracking-wider text-sm md:text-sm sm:hidden">
+          {NAV_LINKS.map((link, index) => (
+            <li key={index}>
+              <a
+                className="no-underline text-[#cbd5e] hover:text-[#0ff] transition-all duration-200"
+                href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
