155f4347ffa8eba531ff5ef636b49a40085e63b2
Baburao Adkane <b.g.adkane27@gmail.com>
2025-05-13 08:02:56 +0530

    model related changes like position/direction

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,6 +1,6 @@
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { useRef, useEffect } from "react";
+import { useRef} from "react";
 
 const Model = () => {
   const model = useGLTF('/models/mushrooms.glb'); 
@@ -8,15 +8,15 @@ const Model = () => {
 
   useFrame(() => {
     ref.current.rotation.y += 0.002;
-    // ref.current.rotation.x += 0.002;
+    // ref.current.rotation.x += 0.0009;
   });  
 
   return (
     <primitive 
     ref={ref} 
     object={model.scene} 
-    scale={1.35}
-    position={[0, -0.50, -1.5]}
+    scale={0.3}
+    position={[0, -0.20, -1]}
     />
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,25 +8,31 @@ import Model from "../components/Model";
 const HeroSection = () => {
   return (
     <div className="hero-container">
-      <Canvas camera={{ position: [0, 0, 4], fov: 60 }}>
+      <Canvas camera={{ position: [0, 0, -2], fov: 60 }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 5]} />        
-        <Stars radius={100} depth={50} count={500} factor={4} />
+        <Stars radius={100} depth={50} count={800} factor={4} />
         {/* <Sphere /> */}
         <Model />
-        <OrbitControls enableZoom={false} />
+        <OrbitControls 
+        enableZoom={false} 
+        />
       </Canvas>
 
       <div className="hero-content">
         <h1 className="uppercase text-nowrap"><strong>Baburao Adkane</strong></h1>
-        <p className="text-[#fff] uppercase leading-1.5">ATTENTIVE | STRONG ANALYTICAL SKILL <br /> Excellent Communication & Collaboration | Process-Oriented</p>
+        <div>
+          <p className="uppercase font-semibold sm:text-xl text-[14px]">QA/QC ENGINEER</p>
+        </div>
+        <p className="uppercase leading-8 text-[10px] sm:text-base">ATTENTIVE | STRONG ANALYTICAL SKILL <br /> 
+        Excellent Communication & Collaboration | Process-Oriented <br />
+        Curious & Proactive</p>
         <div className="hero-links">
           <a href="#work">WORK</a>
-          <a href="#prototypes">PROTOTYPES</a>
-          <a href="#art">ART</a>
-          <a href="#press">PRESS</a>
-          <a href="#info">INFO</a>
-          <a href="#reel">REEL</a>
+          <a href="#experience">EXPERIENCE</a>
+          <a href="#skills">SKLLS</a>
+          <a href="#testimonials">TESTIMONIALS</a>
+          <a href="#contact">CONTACT ME</a>
         </div>
       </div>
     </div>
